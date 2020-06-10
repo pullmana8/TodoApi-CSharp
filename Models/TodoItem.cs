28aa5bb2703630313003aed49f7fe1a5993c5a30
Antonette Caldwell <pullmana8@gmail.com>
2020-06-10 12:12:39 -0500

    .NET TodoApi

---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+
+    public bool isComplete { get; set; }
+
+    public string Secret { get; set; }
+}
